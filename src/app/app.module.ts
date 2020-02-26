59c5accc320560683aa17abb3068eb660503dc05
Danny <mtme13@hotmail.com>
2020-02-26 13:22:57 +0000

    Add Ngxs + tools to App module * register State * add logger plugin * add Redux devtools plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReadComponent } from './components/read/read.component';
 import { CreateComponent } from './components/create/create.component';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
+import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { TutorialState } from './store/tutorial/tutorial.state';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,15 @@ import { CreateComponent } from './components/create/create.component';
     CreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // add State classes here
+    NgxsModule.forRoot([
+      TutorialState
+    ]),
+    // add Redux devtool support
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    // add logger
+    NgxsLoggerPluginModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
