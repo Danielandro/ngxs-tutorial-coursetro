558a050b3ccba4a04334ead34e63ab99bb95796c
Danny <mtme13@hotmail.com>
2020-02-26 11:00:30 +0000

    Create Tutorial actions * add and remove tutorial

---
+++ src/app/actions/tutorial.actions.ts
@@ -0,0 +1,23 @@
+import { Action } from "@ngxs/store";
+import { Tutorial } from '../models/tutorial.model';
+
+/*
+Actions can be:
+-> commands - trigger something to happen
+-> events - event of an action that's already happened
+ */
+
+// Define class with Action name
+// Add static readonly type property
+// if any data is passed, add to constructor as a public property
+
+// Same as NgRx (old syntax, apart from properties on type)
+export class AddTutorial {
+  static readonly type = "[CREATE COMPONENT] Add Tutorial";
+  constructor(public payload: Tutorial) { }
+}
+
+export class RemoveTutorial {
+  static readonly type = "[TUTORIAL] Remove Tutorial";
+  constructor(public payload: string) { }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReadComponent } from './components/read/read.component';
+import { CreateComponent } from './components/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule
